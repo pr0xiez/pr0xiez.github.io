e9052a4950519423449d1c6d59f0dea56a348980
Alex Hall <alex.hallll.03@gmail.com>
2018-04-14 15:06:21 -0400

    initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'All About Hardwood',
+  description: 'Just playing around',
+  themeConfig: {
+    editLinks: true,
+    docsDir: 'docs',
+    nav: [
+      {
+        text: 'Installations',
+        link: 'http://allabouthardwood.com/new-installations/4171597',
+      },
+      {
+        text: 'Dustfree Sanding & Refinishing',
+        link: 'http://allabouthardwood.com/dustfree-sanding-refinishing/4171599'
+      },
+      {
+        text: 'Recoating',
+        link: 'http://allabouthardwood.com/recoating/4541360'
+      }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# All About Hardwood Floor Company
